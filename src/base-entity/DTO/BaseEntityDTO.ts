70f888a9824e5d6f7b156152c876fc846dae9263
AsukFavour <favourasuk@gmail.com>
2022-08-08 13:44:23 +0100

    added base entity

---
+++ src/base-entity/DTO/BaseEntityDTO.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+export class CreateBaseEntityDTO{
+    createdById: string;
+    createdAt: Date;
+    modifyById: string;
+    modifiedAt: Date;
+  }
+  
---
+++ src/base-entity/base-entity.controller.ts
@@ -0,0 +1,45 @@
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+  } from '@nestjs/common';
+  import { BaseEntityService } from './base-entity.service';
+  import { CreateBaseEntityDTO } from './DTO/BaseEntityDTO';
+  
+  @Controller('causeofcrime')
+  export class BaseEntiyController {
+    constructor(private baseEntityService: BaseEntityService) {}
+  
+    @Post()
+    async create(@Body() createDto: CreateBaseEntityDTO) {
+      return this.baseEntityService.create(createDto);
+    }
+  
+    @Patch(':_id')
+    async update(
+      @Param('_id') _Id: string,
+      @Body() updated: CreateBaseEntityDTO,
+    ) {
+      return this.baseEntityService.update(_Id, updated);
+    }
+  
+    @Get()
+    async findAll() {
+      return this.baseEntityService.findAll();
+    }
+  
+    @Get(':_id')
+    async findOne(@Param('_id') _id: string) {
+      return this.baseEntityService.findById(_id);
+    }
+  
+    @Delete(':_id')
+    async delete(@Param('_id') _id: string) {
+      return this.baseEntityService.delete(_id);
+    }
+  }
+  
