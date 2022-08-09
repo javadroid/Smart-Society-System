c0ae8a6c935aee543b8831067c421b5357508d22
Mchim91 <chimenem.mbato99@gmail.com>
2022-08-09 10:14:22 +0100

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,31 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BaseEntityModule } from './base-entity/base-entity.module';
 import { CauseOfCrimeModule } from './cause-of-crime/cause-of-crime.module';
+import { CorrectionCentersModule } from './correction-centers/correction-centers.module';
+import { CourtChargeModule } from './court-charge/court-charge.module';
+import { CrimeOffendersModule } from './crime-offenders/crime-offenders.module';
+import { CrimesModule } from './crimes/crimes.module';
+import { OffencesModule } from './offences/offences.module';
+import { OffendersModule } from './offenders/offenders.module';
+import { PrisonModule } from './prison/prison.module';
+import { PrisonWardModule } from './prison-ward/prison-ward.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/Timeline'),CauseOfCrimeModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/Timeline'),
+    BaseEntityModule,
+    CauseOfCrimeModule,
+    CorrectionCentersModule,
+    CourtChargeModule,
+    CrimeOffendersModule,
+    CrimesModule,
+    OffencesModule,
+    OffendersModule,
+    PrisonModule,
+    PrisonWardModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
